800e6d6f7642a3eeccf50cbda7ce25a6e9b1753d
Megan Nguyen <megantnguyen@utexas.edu>
2020-03-06 11:06:36 -0600

    Add personal finances page js and html

--- donations/ind_script.js
+++ donations/ind_script.js
@@ -11,7 +11,7 @@ client.makeRequest('candIndustry', {cid: 'N00007360', output: 'xml'})
   var result = convert.xml2json(res, {compact: true, spaces: 4});
   var result = JSON.parse(result)
 
-  document.getElementById("header").innerHTML = "Top Ten Industries Contributing to " + result.response.industries._attributes.cand_name + "in 2016 <br>";
+  document.getElementById("header").innerHTML = "Top Ten Industries Contributing to " + result.response.industries._attributes.cand_name + " in 2016 <br>";
 
   var br = document.createElement("br");
   elem.appendChild(br)
@@ -20,7 +20,6 @@ client.makeRequest('candIndustry', {cid: 'N00007360', output: 'xml'})
   for(var i = 0; i < result.response.industries.industry.length; i++) {
         var industry_title = (i+1) + ". " + result.response.industries.industry[i]._attributes.industry_name + "<br>";
         document.getElementById("results").innerHTML += industry_title.bold();
-        // document.getElementById("results").innerHTML += (i+1) + ". " + result.response.industries.industry[i]._attributes.industry_name + "<br>";
         document.getElementById("results").innerHTML += "Total from large individual contributions: " + result.response.industries.industry[i]._attributes.indivs + "<br>";
         document.getElementById("results").innerHTML += "Total from PACs: " + result.response.industries.industry[i]._attributes.pacs + "<br>";
         document.getElementById("results").innerHTML += "Total from all itemized sources: " + result.response.industries.industry[i]._attributes.total + "<br>" + "<br>";
