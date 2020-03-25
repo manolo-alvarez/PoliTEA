71c148796c8d620553b1c3ccdbb4d7db1fd1bbb0
Megan Nguyen <megantnguyen@utexas.edu>
2020-03-25 01:38:43 -0500

    Edit politicians schema

--- RestAPI/server.js
+++ RestAPI/server.js
@@ -1,46 +1,126 @@
-const uri = "mongodb+srv://truther:berniebitches420@cluster0-p5cmn.mongodb.net/test?retryWrites=true&w=majority"
+// const uri = "mongodb+srv://truther:berniebitches420@cluster0-p5cmn.mongodb.net/test?retryWrites=true&w=majority"
+
+const uri = "mongodb+srv://megan:123@clustertest-wsnll.mongodb.net/test?retryWrites=true&w=majority"
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-mongoose.connect(uri, {useNewUrlParser: true});
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => console.log("Connected!"))
 
-const politicianSchema = new mongoose.Schema({
-    first_name: String,
-    last_name: String,
-    house: String,
-    party: String,
-    age: String,
-    bills: [String],
-    donors:{}
+// const politicianSchema = new mongoose.Schema({
+//     crp_id: String,
+//     first_name: String,
+//     last_name: String,
+//     house: String,
+//     party: String,
+//     age: String,
+//     bills: [String],
+//     net_worth: String,
+//     // donors:{}
+//     // donors: [mongoose.ObjectId]
+//     // donors:
+//     //   [{
+//     //     year: String,
+//     //     org_name: String,
+//     //     total: String,
+//     //     pacs: String,
+//     //     indivs: String
+//     //   }]
+//     // }
+//
+// });
 
+
+const politicianSchema = new mongoose.Schema({
+  id: String,
+  title: String,
+  short_title: String,
+  api_uri: String,
+  first_name: String,
+  middle_name: String,
+  last_name: String,
+  suffix: String,
+  date_of_birth: String,
+  gender: String,
+  party: String,
+  leadership_role: String,
+  twitter_account: String,
+  facebook_account: String,
+  youtube_account: String,
+  govtrack_id: String,
+  cspan_id: String,
+  votesmart_id: String,
+  icpsr_id: String,
+  crp_id: String,
+  google_entity_id: String,
+  fec_candidate_id: String,
+  url: String,
+  rss_url: String,
+  contact_form: String,
+  in_office: Boolean,
+  cook_pvi: String,
+  dw_nominate: Number,
+  ideal_point: Number,
+  seniority: String,
+  next_election: String,
+  total_votes: Number,
+  missed_votes: Number,
+  total_present: Number,
+  last_updated: String,
+  ocd_id: String,
+  office: String,
+  phone: String,
+  fax: String,
+  state: String,
+  district: String,
+  at_large: Boolean,
+  geoid: String,
+  missed_votes_pct: Number,
+  votes_with_party_pct: Number,
+  votes_against_party_pct: Number,
+  donors:
+    [{
+      year: String,
+      org_name: String,
+      total: String,
+      pacs: String,
+      indivs: String
+    }]
 });
+
 const locationSchema = new mongoose.Schema({
     //Editing locationSchema how do we define a location??
     district: String,
     events: {},
     polls: {},
 });
+
 const billSchema = new mongoose.Schema({
     issue: String,
     title: String,
     date: Date,
     description: String,
-    politicians_involved: [mongoose.ObjectId] 
+    politicians_involved: [mongoose.ObjectId]
 });
 const politicians = mongoose.model('politicians', politicianSchema);
 const locations = mongoose.model('locations', locationSchema);
 const bills = mongoose.model('bills', billSchema);
 
-let guy = new politicians({
-    first_name: 'Bill',
-    last_name: 'Clinton',
-    house:'Congress',
-    party:'Democrat',
-    Age:'100',
-    bills:['I','didnt','inhale'],
-    donors:['Monica','Lewinsky']
-})
-guy.save();
+// let guy = new politicians({
+//     crp_id: 'N0003',
+//     first_name: 'Bob',
+//     last_name: 'Clinton',
+//     house:'Congress',
+//     party:'Democrat',
+//     Age:'100',
+//     bills:['I','didnt','inhale'],
+//     // donors: [{year: '2020', org_name:'fb', total: '1', pacs: '2', indivs: '3'}, {year: '2020',org_name: 'goog', total: '4', pacs: '5', indivs: '6'}]
+// })
+// guy.save();
+
+module.exports = mongoose.model('politicians', politicianSchema)
