05663dd822b9431e1fee80a58a15bef1aecb592e
Jireh <bailloj@utexas.edu>
2020-03-27 00:02:43 -0500

    added unified topology

--- RestAPI/server.js
+++ RestAPI/server.js
@@ -2,7 +2,7 @@ const uri = "mongodb+srv://truther:berniebitches420@cluster0-p5cmn.mongodb.net/t
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-mongoose.connect(uri, {useNewUrlParser: true});
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => console.log("Connected!"))
