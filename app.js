92ca66d8373408ad64edc08fa4b9b66bc7c52ac4
Megan Nguyen <megantnguyen@utexas.edu>
2020-03-27 18:55:03 -0500

    Add api example

--- app.js
+++ app.js
@@ -1,95 +1,29 @@
-const uri = "mongodb+srv://truther:berniebitches420@cluster0-p5cmn.mongodb.net/test?retryWrites=true&w=majority"
 const express = require('express');
-const app = express();
 const mongoose = require('mongoose');
-mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => console.log("Connected!"))
+const app = express();
+const uri = "mongodb+srv://truther:berniebitches420@cluster0-p5cmn.mongodb.net/test?retryWrites=true&w=majority"
 
-// const politicians = mongoose.model('politicians');
+const postRoutes = require('./routes/api_routes');
 
-const apiroutes = require('./routes/api_routes.js');
-app.use('/', apiroutes); //using routes specified externally
+app.use(express.urlencoded({
+    extended: false
+}));
 
-app.listen(3000, () => console.log(`Example app listening on port 3000!`))
+app.use(express.json());
 
-const politicianSchema = new mongoose.Schema({
-  id: String,
-  title: String,
-  short_title: String,
-  api_uri: String,
-  first_name: String,
-  middle_name: String,
-  last_name: String,
-  suffix: String,
-  date_of_birth: String,
-  gender: String,
-  party: String,
-  leadership_role: String,
-  twitter_account: String,
-  facebook_account: String,
-  youtube_account: String,
-  govtrack_id: String,
-  cspan_id: String,
-  votesmart_id: String,
-  icpsr_id: String,
-  crp_id: String,
-  google_entity_id: String,
-  fec_candidate_id: String,
-  url: String,
-  rss_url: String,
-  contact_form: String,
-  in_office: Boolean,
-  cook_pvi: String,
-  dw_nominate: Number,
-  ideal_point: Number,
-  seniority: String,
-  next_election: String,
-  total_votes: Number,
-  missed_votes: Number,
-  total_present: Number,
-  last_updated: String,
-  ocd_id: String,
-  office: String,
-  phone: String,
-  fax: String,
-  state: String,
-  district: String,
-  at_large: Boolean,
-  geoid: String,
-  missed_votes_pct: Number,
-  votes_with_party_pct: Number,
-  votes_against_party_pct: Number,
-  donors:
-    [{
-      year: String,
-      org_name: String,
-      total: String,
-      pacs: String,
-      indivs: String
-    }],
-  industries:
-    [{
-      year: String,
-      industry_name: String,
-      total: String,
-      pacs: String,
-      indivs: String
-    }],
+app.use('/', postRoutes);
 
-    finances:
-    [{
-        year: String,
-        net_worth: Number,
-        total_assets: Number,
-    }],
-    assets: [{
-      year: String,
-      name: String,
-      asset_value: Number
-    }]
+
+app.get('/', (req, res, next) => {
+    res.send('running node-api');
 });
-const politicians = mongoose.model('politicians', politicianSchema);
 
-module.exports = politicians
+mongoose
+    .connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        app.listen(3000, console.log('Server started at port 3000'));
+    })
+    .catch(err => console.log(err));
--- controller/post_controller.js
+++ controller/post_controller.js
@@ -1,29 +1,11 @@
-//post_controller.js
-const uri = "mongodb+srv://truther:berniebitches420@cluster0-p5cmn.mongodb.net/test?retryWrites=true&w=majority"
-const express = require('express');
-const app = express();
-const mongoose = require('mongoose');
-mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => console.log("Connected!"))
-
-const politicians = require('../app.js')
+const politicians = require('../model/post_model');
 
 exports.showIndex = (req, res, next) => {
        res.send('ruunning node api');
 }
 
-// exports.showPost = (req, res, next) => {
-//       politicians.find() //fetches all the posts
-//          .then(result => {
-//              res.send(result);
-//          }).catch(err => {
-//              res.status(400).send(err);
-//          })
-// }
 
-exports.showPost = (req, res, next) => {
+exports.showPolitician = (req, res, next) => {
   politicians.find({"last_name": "Amash"},  function (err, docs) {
     if (err) return err
     res.status(200).json(docs)
