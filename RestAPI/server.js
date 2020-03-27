5a350171540018cb305f221a56961d8d0935e062
Jireh <bailloj@utexas.edu>
2020-03-27 15:29:47 -0500

    saving

--- RestAPI/server.js
+++ RestAPI/server.js
@@ -6,3 +6,4 @@ mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => console.log("Connected!"))
+
