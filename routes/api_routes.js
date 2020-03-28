cefb92b4c12ef1be456d463ffa55476d27dec787
Megan Nguyen <megantnguyen@utexas.edu>
2020-03-27 19:56:59 -0500

    Add financial info routes and controller functions

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -5,12 +5,14 @@ const router = express.Router();
 
 router.get('/',postcontroller.showIndex)
 
-
-
 router.get('/politician/:id', postcontroller.showPolitician);
 
+router.get('/donors/:id', postcontroller.showDonors);
 
+router.get('/industries/:id', postcontroller.showIndustries);
 
+router.get('/finances/:id', postcontroller.showFinances);
 
+router.get('/assets/:id', postcontroller.showAssets)
 
 module.exports = router;
