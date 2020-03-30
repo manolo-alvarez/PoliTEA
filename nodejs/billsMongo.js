246db66ecf443c16f18237425b81fa60a605ddf4
aidanmcgraw <aidancmcgraw@gmail.com>
2020-03-30 09:06:27 -0500

    Re-populating bills collection

--- nodejs/billsMongo.js
+++ nodejs/billsMongo.js
@@ -1,8 +1,6 @@
-/**
- * @author: Manolo Alvarez
- * @lastRevised: 03/19/2020
- * @summary: Javascript file that pulls politicians ProPublica API and populates MongoDB
- */
+
+
+const mongoose = require('mongoose');
 const bill = require('./billsSchema.js')
 
 // Constant URL value for ProPublica API. This pulls all members in the current (116) house
@@ -11,15 +9,22 @@ const FEDERAL_BILLS_URL = 'https://api.propublica.org/congress/v1/115/both/bills
 // Constant https
 const https = require('https');
 
-mongoose.set('bufferCommands', false);
+//mongoose.set('bufferCommands', false);
 
+
+
+      mongoose.connect('mongodb+srv://truther:berniebitches420@cluster0-p5cmn.mongodb.net/test?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).catch(err => console.log("THIS IS THE PROBLEM"));
+  
 try{
 
-/* 
+
 var i = 0;
 var offset;
 while(i < 13000) {
-    var url = FEDERAL_BILLS_URL + "?offset=" + i; */
+   var url = FEDERAL_BILLS_URL + "?offset=" + i;
 
 
     https.get(url, {headers: {'X-API-Key': 'P3QUvk64v2F2XNUHwHPyhhfqs22CVRE2NVlUvELJ'}}, (resp) => {
@@ -37,7 +42,7 @@ while(i < 13000) {
     resp.on('end', () => {
       var data = JSON.parse(json)
 
-      data.results[i].bills.forEach(member => {
+      data.results[0].bills.forEach(member => {
 
         bill.create(member, function (err, member) {
           if (err) return "error";
@@ -51,9 +56,10 @@ while(i < 13000) {
   }).on("error", function(error) {
         console.log("prob here");
   });
-/*   i = i + 20;
-} */
-} catch(e) {
-    console.log("Problem is with GET");
+  i = i + 20;
 }
+ } catch(e) {
+    console.log("Problem is with GET");
+} 
 
+    
